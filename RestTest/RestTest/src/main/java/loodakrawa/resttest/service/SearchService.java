6b9003f6b868d7482e453d3d6023fe31fba475bc
Luka Sverko <luka.sverko@petcircle.com.au>
2016-05-13 10:15:53 +1000

    Add logging to RestTest

--- RestTest/RestTest/src/main/java/loodakrawa/resttest/service/SearchService.java
+++ RestTest/RestTest/src/main/java/loodakrawa/resttest/service/SearchService.java
@@ -7,19 +7,26 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import lombok.extern.slf4j.Slf4j;
 import loodakrawa.resttest.dal.SearchDAO;
 import loodakrawa.resttest.model.Query;
 import loodakrawa.resttest.model.Result;
 
+@Slf4j
 @Path("search")
 public class SearchService {
 
-	@Inject
 	private SearchDAO dao;
-	
+
+	@Inject
+	public SearchService(SearchDAO dao) {
+		this.dao = dao;
+	}
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Result process(@QueryParam("query") String searchString) {
+		log.info("Searching for: " + searchString);
 		Query query = new Query(searchString);
 		return dao.search(query);
 	}
